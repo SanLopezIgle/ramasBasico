dd1527c38f6dfcc0f889c393e0158f6447753cd7
Sandra López Iglesias <slopeziglesias@danielcastelao.org>
2023-02-23 19:17:18 +0100

    modificando .gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Commit A");
+        System.out.println("Commit B");
+
+        System.out.println("Commit E");
+    }
+}
